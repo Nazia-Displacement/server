8bcac137de714add64172873be79ed4be3c733e1
Isaac <thetornadotitan@gmail.com>
2024-06-02 22:24:58 -0400

    add Camera_App -> Unity comms

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // # Imports
 // ############################################################################
 const express = require("express");
+const { json } = require("express/lib/response");
 const app = express();
 const { Server } = require("socket.io");
 const io = new Server({
@@ -15,10 +16,16 @@ require("dotenv").config();
 // ############################################################################
 io.use((socket, next) => {
   console.log("Connection being attempted");
+  console.log(socket.handshake.query);
   if (socket.handshake.query.token === process.env.UNITYKEY) next();
   else if (
     socket.handshake.query.token === process.env.MIDIKEY &&
     socket.handshake.query.secret === process.env.MIDISECRET
+  )
+    next();
+  else if (
+    socket.handshake.query.token === process.env.CAMKEY &&
+    socket.handshake.query.secret === process.env.CAMSECRET
   )
     next();
   else {
@@ -75,6 +82,22 @@ io.sockets.on("connection", (socket) => {
         console.log("Emit 176");
         io.emit("door-on", msg.message[2]);
       }
+    });
+
+    return;
+  }
+
+  // CAM connected
+  if (socket.handshake.query.token === process.env.CAMKEY) {
+    console.log("CAM Connected");
+
+    socket.on("disconnect", (args) => {
+      console.log("CAM Disconnected");
+    });
+
+    socket.on("keyframes", (k) => {
+      console.log(k);
+      io.emit("ukeyframes", k);
     });
 
     return;
